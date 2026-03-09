ab00fb18caab42e302f9536e37fdec29ed770167
Toffee <160152673+toffeecaramel@users.noreply.github.com>
2026-03-08 22:28:16 -0300

    Test commit!

--- Prebuild.hx
+++ Prebuild.hx
@@ -10,31 +10,10 @@ class Prebuild
 {
     static inline final BUILD_TIME_FILE:String = '.build_time';
 
-    public static var compileMsgs:Array<String> = [
-        "We love legacy mods. VS Hex my beloved <3...",
-        "Did you know that the chart editor is awesome because Luna is also awesome?",
-        "If you ever see a moon walking on the streets, remember to scream MOON ENGINE!!!",
-        "Luna mix is cool",
-        "Mano mix is cool",
-        "Mad Virus Attack is pawesome",
-        "Cool Pokemon Mod is cool",
-        "Vee Mix is the coolest",
-        "just add an random useless fact idk i cant think of anything :sob: -- zzshu",
-        "The evoker can change the color of the wool of the blue sheeps",
-        "Easier to assimilate than explain, anyway.",
-        "A mod we gave to bug and beast as they had never dreamed"
-    ];
-
     static function main():Void
     {
         saveBuildTime();
-        traceMessage();
-    }
-
-    public static function traceMessage():Void
-    {
-        final message = compileMsgs[Std.random(compileMsgs.length)];
-        Sys.println('\x1b[36m[INFO] Today\'s message: $message\x1b[0m');
+        Sys.println('\x1b[33m[WARNING] YOU\'RE ON A EXPERIMENTAL BRANCH, EXPECT BUGS.\x1b[0m');
     }
 
     static function saveBuildTime():Void
